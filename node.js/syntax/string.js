e305a1874a1c8b7b877b97336b1560760a866be8
JoYoonJae <48471177+JoYoonJae@users.noreply.github.com>
2020-01-22 19:14:19 +0900

    02 commit
    javascript string

---
+++ node.js/syntax/string.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');
+console.log('JavaScript (/ˈdʒɑːvəˌskrɪpt/),[9] often abbreviated as JS, is a high-level, just-in-time compiled, multi-paradigm programming language that conforms to the ECMAScript specification.[10] JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.'.length);
