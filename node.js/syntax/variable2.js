ac64abba060d5b5ac0bda9993b3457753e4daa4a
JoYoonJae <ynj3126@naver.com>
2020-01-22 23:54:04 +0900

    04 commit
    query string 까지

---
+++ node.js/syntax/variable2.js
@@ -0,0 +1,3 @@
+var name = 'egoing';
+var letter = 'JavaScript '+name+' (/ˈdʒɑːvəˌskrɪpt/),[9] often abbreviated as JS, is a high-level, just-in-time compiled, multi-paradigm '+name+'programming language that conforms to the '+name+' ECMAScript specification.[10] JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.';
+console.log(letter);
